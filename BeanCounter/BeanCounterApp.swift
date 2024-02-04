4c5e4adaaf2455d6ad2d4e313ad5c4c3142f9920
Tom Pritchard <pritch90@gmail.com>
2024-02-04 22:03:54 +0000

    Initial Commit

---
+++ BeanCounter/BeanCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanCounterApp.swift
+//  BeanCounter
+//
+//  Created by Tom Pritchard on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BeanCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
